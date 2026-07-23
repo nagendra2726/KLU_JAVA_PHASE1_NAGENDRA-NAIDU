83b3441f8077314d560e758b62b375464a3b9c79
Chellu Nagendra Vara Prasad <nagendrachellu7@gmail.com>
2026-07-23 14:31:56 +0530

    Add files via upload

---
+++ Day1 & 2.java
@@ -0,0 +1,24 @@
+//Leap Year or Not 
+
+
+// import java.util.Scanner;
+// class Main {
+//     public static void main(String[] args){
+//         Scanner sc =new Scanner(System.in);
+//         int n = sc.nextInt();
+//         if(n%400 == 0 || (n%4 == 0 && n%100 != 0)){
+//             System.out.print("Leap Year");
+//     } else {
+//             System.out.print("Not a Leap Year");
+
+
+
+            
+//         }
+//     }
+// }
+
+
+// Output
+// 2026
+// Not a Leap Year
